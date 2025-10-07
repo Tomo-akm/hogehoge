89e4c8ee66143235db1af3bcfd3db76c146dc46c
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-10-07 16:58:53 +0900

    feat: implement-slim-template and BS5

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Hogetaro
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.template_engine = :slim
   end
 end
